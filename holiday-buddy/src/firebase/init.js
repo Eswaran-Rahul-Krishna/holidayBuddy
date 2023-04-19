d9cee0baf817cdef5782e97817a4cef886a2b101
Eswaran-Rahul-Krishna <eswaran.krishnamurthy@outlook.com>
2023-04-19 17:23:12 +0100

    Update init.js

--- holiday-buddy/src/firebase/init.js
+++ holiday-buddy/src/firebase/init.js
@@ -8,21 +8,13 @@ import Filter from 'bad-words'
 import { ref, onUnmounted, computed } from 'vue'
 
 firebase.initializeApp({
-
   apiKey: "AIzaSyBpEvYA9KbQywv2SzLnf-FlFuK9n0z6YII",
-
   authDomain: "test-351f2.firebaseapp.com",
-
   projectId: "test-351f2",
-
   storageBucket: "test-351f2.appspot.com",
-
   messagingSenderId: "615010128511",
-
   appId: "1:615010128511:web:4c6c0123c2ab0c6a05654a",
-
   measurementId: "G-4E5ST03XZZ"
-
  });
 
 const auth = firebase.auth()
