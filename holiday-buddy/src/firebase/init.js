0da26b5b51c77a17fc0d19f5c12044e1616fa150
Eswaran-Rahul-Krishna <126084585+Eswaran-Rahul-Krishna@users.noreply.github.com>
2023-04-19 17:13:44 +0100

    Update init.js

--- holiday-buddy/src/firebase/init.js
+++ holiday-buddy/src/firebase/init.js
@@ -8,14 +8,22 @@ import Filter from 'bad-words'
 import { ref, onUnmounted, computed } from 'vue'
 
 firebase.initializeApp({
-    apiKey: "AIzaSyBpEvYA9KbQywv2SzLnf-FlFuK9n0z6YII",
-    authDomain: "test-351f2.firebaseapp.com",
-    projectId: "test-351f2",
-    storageBucket: "test-351f2.appspot.com",
-    messagingSenderId: "615010128511",
-    appId: "1:615010128511:web:38119dce23470ec905654a",
-    measurementId: "G-WFYR4YBGJK"
-  })
+
+  apiKey: "AIzaSyBpEvYA9KbQywv2SzLnf-FlFuK9n0z6YII",
+
+  authDomain: "test-351f2.firebaseapp.com",
+
+  projectId: "test-351f2",
+
+  storageBucket: "test-351f2.appspot.com",
+
+  messagingSenderId: "615010128511",
+
+  appId: "1:615010128511:web:4c6c0123c2ab0c6a05654a",
+
+  measurementId: "G-4E5ST03XZZ"
+
+ });
 
 const auth = firebase.auth()
 
